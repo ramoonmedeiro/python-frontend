6cf36f14428f803fe78762cd407f75ef419eb75d
Ramon Medeiro <r.medeiro10@gmail.com>
2025-10-09 01:26:42 -0300

    feat: inserindo nav bar do spotify

---
+++ spotify-clone/main.py
@@ -0,0 +1,31 @@
+# Reactpy core
+from reactpy import component
+from reactpy_router import browser_router, route
+
+# fastapi config
+from reactpy.backend.fastapi import configure
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+# Components
+from components.health import Health
+from components.index import Index
+
+
+STATIC_DIR = Path(__file__).parent / "static"
+
+@component
+def Route():
+    return browser_router(
+        route("/health", Health()),
+        route("/", Index())
+    )
+
+app = FastAPI()
+app.mount(
+    "/static", 
+    StaticFiles(directory=STATIC_DIR), 
+    name="static"
+)
+configure(app=app, component=Route)
