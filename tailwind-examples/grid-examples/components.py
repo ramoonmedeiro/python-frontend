c62cb67370a1d54cad587f5c39ad00fdcb07d5da
Ramon Medeiro <r.medeiro10@gmail.com>
2025-10-21 18:07:29 -0300

    feat: subindo exemplos de grid

--- tailwind-examples/grid-examples/components.py
+++ tailwind-examples/grid-examples/components.py
@@ -3,10 +3,11 @@ cdn = html.script({"src": "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"}
 
 @component
 def Home():
-    return html.div(
+    return html._(
         cdn,
+        # Colunas no grid
         html.div(
-            {"class": "grid bg-red-300 grid-cols-3"},
+            {"class": "grid grid-cols-3 bg-red-400 m-4"},
             html.div(
                 "1"
             ),
@@ -17,77 +18,86 @@ def Home():
                 "3"
             )
         ),
-        # Grid col-span
         html.div(
-            {"class": "grid grid-cols-6 m-2"},
+            {"class": "grid grid-cols-2 bg-blue-500 m-4"},
             html.div(
-                {"class": "col-span-1 bg-yellow-500"},
+                "1"
+            ),
+            html.div(
+                "2"
+            )
+        ),
+        # Tamanho das colunas
+        html.div(
+            {"class": "grid grid-cols-6 m-3"},
+            html.div(
+                {"class": "col-span-1 bg-yellow-300"},
                 html.ul(
                     html.li("Home"),
                     html.li("Produtos"),
                     html.li("Contato"),
-                    html.li("Blog")
+                    html.li("Blog"),
                 )
             ),
             html.div(
-                {"class": "col-span-5 bg-blue-500"},
-                html.p(
-                    "Loremdfisdusdfhshfhsdfuihsdiufhsdifuhsdiufhdsiufhsduifhdsuif"
-                )
+                {"class": "col-span-5 bg-blue-300"},
+                html.p("""
+                    Why do we use it?
+                    It is a long established fact that 
+                    a reader will be distracted by the readable content of a page when looking at its layout. 
+                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+                    as opposed to using 'Content here, content here', making it look like readable English.
+                """)
             )
         ),
         html.div(
             {"class": "grid grid-cols-4 m-2"},
             html.div(
-                {"class": "col-span-1 bg-red-700"},
-                html.p("Barra lateral esquerda")
+                {"class": "col-span-1 m-1 bg-red-400"},
+                html.p("Barra Lateral Esquerda")
             ),
             html.div(
-                {"class": "col-span-2 bg-red-300"},
-                html.p("Conteúdo")
+                {"class": "col-span-2 m-1 bg-red-200"},
+                html.p("""
+                    Why do we use it?
+                    It is a long established fact that 
+                    a reader will be distracted by the readable content of a page when looking at its layout. 
+                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+                    as opposed to using 'Content here, content here', making it look like readable English.
+                """)
             ),
             html.div(
-                {"class": "col-span-1 bg-red-700"},
-                html.p("Barra lateral direita")
+                {"class": "col-span-1 m-1 bg-red-400"},
+                html.p("Barra Lateral Direita")
             )
         ),
-        # Rows no grid
         html.div(
-            {"class": "grid grid-rows-4 grid-flow-col"},
+            {"class": "grid grid-cols-4 p-2"},
             html.div(
-                {"class": "bg-green-400"},
-                "1"
+                {"class": "col-span-4 bg-green-400 text-white"},
+                html.p("Barra de Navegação")
             ),
             html.div(
-                {"class": "bg-green-800"},
-                "2"
+                {"class": "col-span-1 bg-cyan-500"},
+                html.ul(
+                    html.li("Home"),
+                    html.li("Perfil"),
+                    html.li("API Key"),
+                    html.li("Logout")
+                )
             ),
             html.div(
-                {"class": "bg-green-400"},
-                "3"
-            ),
-            html.div(
-                {"class": "bg-green-800"},
-                "4"
-            ),
-            html.div(
-                {"class": "bg-green-400"},
-                "5"
-            ),
-            html.div(
-                {"class": "bg-green-800"},
-                "6"
-            ),
-            html.div(
-                {"class": "bg-green-400"},
-                "7"
-            ),
-            html.div(
-                {"class": "bg-green-800"},
-                "8"
-            ),
+                {"class": "col-span-3 bg-cyan-300"},
+                html.p("""
+                    Why do we use it?
+                    It is a long established fact that 
+                    a reader will be distracted by the readable content of a page when looking at its layout. 
+                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+                    as opposed to using 'Content here, content here', making it look like readable English.
+                """)
+            )
         ),
-        # Gap
+        # Espaçamento de colunas e linhas
         html.div(
             {"class": "grid grid-cols-3 m-4 gap-2"},
             html.div(
@@ -95,36 +105,25 @@ def Home():
                 "1"
             ),
             html.div(
-                {"class": "bg-purple-500"},
+                {"class": "bg-purple-400"},
                 "2"
             ),
             html.div(
-                {"class": "bg-purple-300"},
+                {"class": "bg-purple-500"},
                 "3"
             ),
             html.div(
-                {"class": "bg-purple-500"},
+                {"class": "bg-purple-300"},
                 "4"
             ),
             html.div(
-                {"class": "bg-purple-300"},
+                {"class": "bg-purple-400"},
                 "5"
             ),
             html.div(
                 {"class": "bg-purple-500"},
                 "6"
             ),
-            html.div(
-                {"class": "bg-purple-300"},
-                "7"
-            ),
-            html.div(
-                {"class": "bg-purple-500"},
-                "8"
-            ),
-            html.div(
-                {"class": "bg-purple-300"},
-                "9"
-            )
+
         )
     )
