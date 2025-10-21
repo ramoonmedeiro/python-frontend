eaa10a90a65fe5b1e6f70b15778b9255e81806f0
Ramon Medeiro <r.medeiro10@gmail.com>
2025-10-21 17:31:23 -0300

    feat: inserindo aula de flexbox

--- tailwind-examples/flexbox-examples/components.py
+++ tailwind-examples/flexbox-examples/components.py
@@ -3,111 +3,94 @@ cdn = html.script({"src": "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"}
 
 @component
 def Home():
-    return html.div(
+    return html._(
         cdn,
-        # flex box em linha
+        # Flex directions
+        ## Flex em linha
         html.div(
-            {"class": "flex flex-row bg-red-300"},
+            {"class": "flex flex-row bg-red-400 m-4"},
             html.div(
-                {"class": "bg-red-600 p-6 m-2"},
+                {"class": "bg-blue-300 p-6 m-2"},
                 "1"
             ),
             html.div(
-                {"class": "bg-red-600 p-6 m-2"},
+                {"class": "bg-blue-300 p-6 m-2"},
                 "2"
             ),
             html.div(
-                {"class": "bg-red-600 p-6 m-2"},
+                {"class": "bg-blue-300 p-6 m-2"},
                 "3"
-            )        
+            )
         ),
-        #flex box em coluna
+        ## Flex em colunas
         html.div(
-            {"class": "flex flex-col bg-blue-300"},
+            {"class": "flex flex-col bg-blue-400 m-4 p-6"},
             html.div(
-                {"class": "bg-blue-600 p-6 m-2"},
+                {"class": "bg-red-400 my-2"},
                 "1"
             ),
             html.div(
-                {"class": "bg-blue-600 p-6 m-2"},
+                {"class": "bg-red-400 my-2"},
                 "2"
             ),
             html.div(
-                {"class": "bg-blue-600 p-6 m-2"},
+                {"class": "bg-red-400 my-2"},
                 "3"
-            )        
+            )
         ),
-        # Exercício 1 do flexbox
-        # Criar container com flex col, 5 itens com cores diferentes
         html.div(
-            {"class": "flex flex-col bg-teal-900"},
+            {"class": "flex flex-col bg-cyan-200 m-2 p-6 border-3 border-blue-400"},
             html.div(
-                {"class": "bg-green-400 p-3 m-2"},
+                {"class": "bg-cyan-300 my-1"},
                 "1"
             ),
             html.div(
-                {"class": "bg-red-400 p-3 m-2"},
+                {"class": "bg-cyan-400 my-1"},
                 "2"
             ),
             html.div(
-                {"class": "bg-pink-400 p-3 m-2"},
+                {"class": "bg-cyan-500 my-1"},
                 "3"
             ),
             html.div(
-                {"class": "bg-blue-400 p-3 m-2"},
+                {"class": "bg-cyan-600 my-1"},
                 "4"
             ),
             html.div(
-                {"class": "bg-purple-400 p-3 m-2"},
+                {"class": "bg-cyan-700 my-1"},
                 "5"
+            ),
+            html.div(
+                {"class": "bg-cyan-800 my-1"},
+                "6"
             )
         ),
         # Wrap
         html.div(
-            {"class": "flex flex-row flex-wrap bg-red-300"},
+            {"class": "flex flex-wrap bg-red-300"},
             html.div(
-                {"class": "bg-green-400 p-3 m-2"},
+                {"class": "bg-blue-300 p-6 m-4"},
                 "1"
             ),
             html.div(
-                {"class": "bg-red-400 p-3 m-2"},
+                {"class": "bg-blue-300 p-6 m-4"},
                 "2"
             ),
             html.div(
-                {"class": "bg-pink-400 p-3 m-2"},
+                {"class": "bg-blue-300 p-6 m-4"},
                 "3"
             ),
             html.div(
-                {"class": "bg-blue-400 p-3 m-2"},
+                {"class": "bg-blue-300 p-6 m-4"},
                 "4"
             ),
             html.div(
-                {"class": "bg-purple-400 p-3 m-2"},
+                {"class": "bg-blue-300 p-6 m-4"},
                 "5"
-            )
-        ),
-        # Order
-        html.div(
-            {"class": "flex bg-teal-300"},
-            html.div(
-                {"class": "bg-purple-400 p-3 m-2 order-last"},
-                "1"
             ),
             html.div(
-                {"class": "bg-purple-400 p-3 m-2 order-4"},
-                "2"
-            ),
-            html.div(
-                {"class": "bg-purple-400 p-3 m-2 order-3"},
-                "3"
-            ),
-            html.div(
-                {"class": "bg-purple-400 p-3 m-2 order-first"},
-                "4"
-            ),
-            html.div(
-                {"class": "bg-purple-400 p-3 m-2 order-2"},
-                "5"
+                {"class": "bg-blue-300 p-6 m-4"},
+                "6"
             )
         )
     )
