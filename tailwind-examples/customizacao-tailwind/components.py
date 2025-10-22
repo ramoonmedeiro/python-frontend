79ec038a81763b8426e23a5cd67bd85c1411d69f
Ramon Medeiro <r.medeiro10@gmail.com>
2025-10-22 11:52:39 -0300

    feat: inserindo aula de customização do tailwind

--- tailwind-examples/customizacao-tailwind/components.py
+++ tailwind-examples/customizacao-tailwind/components.py
@@ -1,14 +1,5 @@
 from reactpy import html, component
 from reactpy_utils import DocumentTitle
-import json
-from tailwind_config import TailwindConfig
-
-
-tailwind_config_json = json.dumps(TailwindConfig.custom_config.value, indent=4)
-tailwind_config_script = html.script(
-    {"type": "text/tailwindcss-config"},
-    tailwind_config_json
-)
 
 styles = html.link({"href": "static/style.css", "rel": "stylesheet"})
 cdn = html.script({"src": "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"})
@@ -18,7 +9,17 @@ def Root():
     return html._(
         cdn,
         styles,
-        tailwind_config_script,
         DocumentTitle("Site básico"),
-        html.p("Tá no ar porra")
+        html.p(
+            {"class": "text-[#ff6347] text-base tablet:text-xl"}, 
+            "Tá no ar porra"
+        ),
+        html.p(
+            {"class": "text-cyan-900"},
+            "Outro texto"
+        ),
+        html.p(
+            {"class": "text-yellow-900 p-[20rem]"},
+            "Outro texto customizado"
+        )
 )
